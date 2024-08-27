4038ed5df0274f109873b7c70108ce6753f76c54
Luther Hlungwani <lhhlungwani3@gmail.com>
2024-08-27 19:40:09 +0200

    broken code

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+let input = ""
+
+function validateSyntax(input) {
+    let input = "";
     // Validation logic goes here
+    if (input.startsWith("pet_")){
+        console.log('go')
+    } 
+
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
